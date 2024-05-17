b2da064b395d3c11b0a9da9714deecb15d8ddf56
Robin Hagmayer <robin.hagmayer@proton.me>
2024-05-17 23:20:30 +0200

    Added an autocommand to resize help splits

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -7,3 +7,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Set the height of the help window to 75% of the total window height
+vim.api.nvim_create_autocmd('BufWinEnter', {
+  pattern = '*',
+  callback = function()
+    if vim.bo.filetype == 'help' then
+      local win_id = vim.api.nvim_get_current_win()
+      local current_win_height = vim.api.nvim_win_get_height(win_id)
+      local new_win_height = math.floor(current_win_height * 1.5)
+      vim.api.nvim_win_set_height(win_id, new_win_height)
+    end
+  end,
+})
