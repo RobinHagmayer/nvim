eb11149416d22b11fd9cf500638dd7e845173936
Robin Hagmayer <robin.hagmayer@proton.me>
2024-05-28 20:03:16 +0200

    feat: Added c++ lsp and inlay hint keymap

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -67,6 +67,11 @@ return {
         --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+        map('<leader>ih', function()
+          ---@diagnostic disable-next-line: missing-parameter
+          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+        end, 'Toggle [I]nlay [H]ints')
+
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
@@ -86,7 +91,7 @@ return {
         end
 
         -- if client and client.server_capabilities.inlayHintProvider then
-        --   vim.lsp.inlay_hint.enable(event.buf, true)
+        --   vim.lsp.inlay_hint.enable(true)
         -- end
       end,
     })
@@ -108,7 +113,7 @@ return {
     --  - settings (table): Override the default settings passed when initializing the server.
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
-      -- clangd = {},
+      clangd = {},
       -- gopls = {},
       -- pyright = {},
       -- rust_analyzer = {},
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -11,6 +11,7 @@ return {
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      ['<leader>i'] = { name = 'Toggle [I]nlay [H]ints', _ = 'which_key_ignore' },
     }
   end,
 }
